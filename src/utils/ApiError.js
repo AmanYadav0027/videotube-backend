2d6529d156edeb50963bf51d9b1febe73d8e92d8
AmanYadav0027 <amanas0027@gmail.com>
2026-02-04 12:42:07 +0530

    util code

---
+++ src/utils/ApiError.js
@@ -0,0 +1,24 @@
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        message = "something went wrong",
+        errors = [],
+        statck = ""
+    ) {
+        super(message);
+        this.statusCode =
+            statusCodethis.data =
+            nullthis.message =
+            messagethis.success =
+                false;
+        this.errors = errors;
+
+        if (statck) {
+            this.stack = statck;
+        } else {
+            error.captureStackTrace(this, this.constructor);
+        }
+    }
+}
+
+export { ApiError };
