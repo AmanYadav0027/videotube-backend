810c94681d5c3fa104818bdbfb06c765bae60799
AmanYadav0027 <amanas0027@gmail.com>
2026-05-10 13:40:12 +0530

    fix: add sameSite none for cross-origin cookies

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -229,6 +229,7 @@ const loginUser = asyncHandler(async (req, res) => {
     const options = {
         httpOnly: true,
         secure: true,
+        sameSite: "none",
     };
 
     return res
@@ -268,6 +269,7 @@ const logoutUser = asyncHandler(async (req, res) => {
     const options = {
         httpOnly: true,
         secure: true,
+        sameSite: "none",
     };
 
     return res
@@ -313,6 +315,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
         const options = {
             httpOnly: true,
             secure: true,
+            sameSite: "none",
         };
 
         const { accessToken, newRefreshToken } =
